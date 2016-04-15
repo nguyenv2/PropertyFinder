f95aef4edb8380ec45a263682a009826cf02baa2
Van Nguyen <van.nguyen@washpost.com>
2016-04-15 12:04:14 -0400

    initial native react app

---
+++ index.ios.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var React = require('react-native');
+
+var styles = React.StyleSheet.create({
+  text: {
+    color: 'blue',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  },
+  container: {
+    flex: 1
+  }
+})
+
+class HelloWorld extends React.Component {
+  render() {
+
+    //return React.createElement(React.Text, {style: styles.text}, "Hello World!");
+
+    return <React.Text style={styles.text}>Hello World</React.Text>;
+
+  }
+}
+
+class PropertyFinderApp extends React.Component {
+  render(){
+    return(
+      <React.NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Property Finder',
+          component: HelloWorld
+        }}
+      />
+    )
+  }
+}
+
+
+React.AppRegistry.registerComponent('PropertyFinder', function() { return HelloWorld });
